2ac5f909d4bf71e44958fb6e6551b116887cce89
7luizarthu <contatoarthur088@gmail.com>
2026-09-13 16:59:41 -0300

    Avançando no projeto back-end

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-print("Sabor-Express\n")
+print("""
+𝑆𝑎𝑏𝑜𝑟 𝐸𝑥𝑝𝑟𝑒𝑠𝑠
+""")
 
 print("1. Cadastrar restaurante")
 print("2. Lista restaurante")
 print("3. Ativar restaurante")
-print("4. Sair")
+print("4. Sair\n")
 
-input('Escolha uma opção: ')
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção{opcao_escolhida}')
