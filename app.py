7845176bb5212cd187c3f44f98dd86bfc028130f
7luizarthu <contatoarthur088@gmail.com>
2026-09-14 20:15:00 -0300

    Finalizando o projeto criado, com toda a documentação do código

--- app.py
+++ app.py
@@ -4,31 +4,48 @@ restaurantes = [{'nome': 'Bar do Paulo', 'categoria': 'Bar', 'ativo': False}, {'
 
 
 def exibir_nome_do_programa():
+    '''Logotipo do App'''
     print("""
     𝑆𝑎𝑏𝑜𝑟 𝐸𝑥𝑝𝑟𝑒𝑠𝑠
     """)
 
 def exibir_menu():
+    '''Função responsável por criar o menu de lista do restaurante'''
     print("1. Cadastrar restaurante")
     print("2. Listar restaurantes")
-    print("3. Ativar restaurante")
+    print("3. Alternar estado do restaurante")
     print("4. Sair\n")
 
 def finalizar_app():
+    '''Função para encerrae encerrar o programa, quando necessário'''
     exibir_submenu('Finalizando o programa...')
 
 def voltar_menu():
+    '''Função criada com intuito de facilitar a volta ao menu principal pelo usuario'''
     input('\nPressione ENTER para voltar ao menu principal...')
     main()
 
 def opcao_invalida():
+    '''Função para informar uma opção inválida ao usuario'''
     print('Opção inválida. Tente novamente.')
     voltar_menu()
 def exibir_submenu(texto):
+    '''Função para exibir novamente o menu'''
     os.system('cls')
+    linha = '*' * (len(texto) + 4)
+    print(linha)
     print(texto)
+    print(linha)
+    print()
 
 def cadastrar_restaurante():
+    '''Essa função é responsável por cadastrar um novo restaurante
+    Inputs: 
+    - Nome do Restaurante;
+    - Categoria.
+    Output:
+    - Adiciona um novo restaurante  a lista de restaurante.
+    '''
     exibir_submenu('Cadastro de restaurantes')
     nome_restaurante = input('Digite o nome do restaurante: ')
     categoria_restaurante = input(f'Digite a categoria do restaurante "{nome_restaurante}": ')
@@ -38,15 +55,18 @@ def cadastrar_restaurante():
     voltar_menu()
 
 def listar_restaurantes():
+    '''Função para realizar a listagem de todos os restaurantes cadastrados'''
     exibir_submenu('Lista de restaurantes')
+    print(f'{'Nome do Restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | {'Status'}')
     for restaurante in restaurantes:
         nome_restaurante = restaurante['nome']
         categoria_restaurante = restaurante['categoria']
-        ativo_restaurante = restaurante['ativo']
-        print(f'- {nome_restaurante} | {categoria_restaurante} | {ativo_restaurante} - {ativo_restaurante}')
+        ativo = "Ativado" if restaurante['ativo'] else 'Desativado'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria_restaurante.ljust(20)} | {ativo}')
     voltar_menu()
 
 def alternar_status_restaurante():
+    '''Função criada para o usuario alterar o estado daquele restaurante no app'''
     exibir_submenu('Ativando/Desativando restaurante')
     nome_restaurante = input('Digite o nome do restaurante que deseja ativar/desativar: ')
     restaurante_encontrado = False
@@ -63,6 +83,7 @@ def alternar_status_restaurante():
         voltar_menu()
 
 def escolher_opcao():
+    '''Função para validar a opção escolhida pelo usuario, e direcionar para o submenu correto'''
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
         if opcao_escolhida == 1:
@@ -79,10 +100,12 @@ def escolher_opcao():
         opcao_invalida()
 
 def main():
+    '''Define a ordem das etapas que aparece ao usuario'''
     os.system('cls')
     exibir_nome_do_programa()
     exibir_menu()
     escolher_opcao()
 
 if __name__ == '__main__':
+    '''Defini a main como arquivo principal'''
     main()
